f5ae3d71d59226b40aa2ba44325326cc2369c6a8
Oakar Kyaw <oakkar8@gmail.com>
2018-05-01 00:27:29 -0700

    DjangoRR, first commit

---
+++ rapidrite/static/scripts/footer.js
@@ -0,0 +1,9 @@
+document.write(
+    	'<ul>' +
+    		'<li><a href="homepage.html">Home</a></li>' +
+    		'<li><a href="#about.html">About</a></li>' +
+    		'<li><a href="#contacts">Contacts</a></li>' +
+    		'<li><a href="#faq">FAQ</a></li>' +
+    		'<li><a href="#privacy">Privacy</a></li>' +	
+		'</ul>'
+		);
