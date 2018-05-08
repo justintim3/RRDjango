192b081c30fdc66c62dcf4bf31d525fa0f7bd989
oakkar8 <35274540+oakkar8@users.noreply.github.com>
2018-05-07 21:52:30 -0700

    Revert "DjangoRR, first commit"
    This reverts commit f5ae3d71d59226b40aa2ba44325326cc2369c6a8.

--- rapidrite/static/scripts/footer.js
+++
@@ -1,9 +0,0 @@
-document.write(
-    	'<ul>' +
-    		'<li><a href="homepage.html">Home</a></li>' +
-    		'<li><a href="#about.html">About</a></li>' +
-    		'<li><a href="#contacts">Contacts</a></li>' +
-    		'<li><a href="#faq">FAQ</a></li>' +
-    		'<li><a href="#privacy">Privacy</a></li>' +	
-		'</ul>'
-		);
