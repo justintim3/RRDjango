4a1c0ad04107b7ce77defe46bcb8ddacd8c4d4a8
Justin Lin <justlin90@gmail.com>
2018-05-09 02:42:15 -0700

    Fixed a bug in the apps.py file where the path and default_app_config properties were not set.

--- RRcomic/website/apps.py
+++ RRcomic/website/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class WebsiteConfig(AppConfig):
     name = 'website'
+    path = '/opt/python/current/app/website'
+    default_app_config 'website.apps.WebsiteConfig'
