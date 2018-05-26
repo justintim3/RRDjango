f3752b3c0515ed8f0c16c0be68b10d481f8253fd
justintim3 <justlin90@gmail.com>
2018-05-26 02:46:42 -0700

    Justin (#19)
    * Fixed rating system.
    
    * Minor changes
    
    * Added changes to publisher and series pages
    
    * Alphabetized lists for series and publisher pages. Updated design for several pages. Added follow/unfollow button for profile page.
    
    * added comicsearchresults page
    
    * added forms.py
    
    * added forms.py
    
    * modified comicsearchresults.html
    
    * added search bar
    
    * added search bar
    
    * added pages for search by title, character, creator
    
    * added dropdown script
    
    * added functional search by title
    
    * Added search bar to main bar for all pages
    
    * Fixed issue with logo on search page
    
    * Fixed logo for login/logged_out pages.
    
    * Disabled button css on comicpage
    
    * Disabled button css on profile page.
    
    * Updated search bar. Renamed or added titles to some pages.
    
    * Added timeline rating and review items.
    
    * Readded files
    
    * Deleted files.
    
    * Added timeline onto profile page
    
    * Merged and resolved conflicts

--- website/views.py
+++ website/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from .models import *
-from .forms import *
 from django.contrib.auth.forms import UserCreationForm
 from django.utils import timezone
 from django.db import connection
